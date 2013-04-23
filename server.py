638032e1901d5bc013c4efee1fe9b9c805b18213
Xiaonuo Gantan <xiaonuo.gantan@browzoo.com>
2013-04-23 14:33:16 -0600

    Initial commit

---
+++ server.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import, division
+import flask
+
+app = flask.Flask(__name__)
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ test/requests_test.py
@@ -0,0 +1,3 @@
+"""Test whether requests can be made against reddit's API endpoints."""
+
+from server import app
